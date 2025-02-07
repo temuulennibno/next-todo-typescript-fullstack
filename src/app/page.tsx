2fb0031a3d858bee745c63cfc4f34c3374dbd7b0
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2025-02-07 16:15:38 +0800

    add react hook form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,35 @@
 "use client";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Todo } from "@/types/todo";
 import { useEffect, useState } from "react";
 import { TodoItem } from "./components/todo-item";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { toast } from "sonner";
+import axios from "axios";
+import { todo } from "node:test";
+
+const FormSchema = z.object({
+  title: z
+    .string()
+    .min(2, { message: "Todo title must have at least 2 characters" })
+    .max(255, { message: "Todo title can't have more than 255 characters" }),
+});
+
 export default function Home() {
   const [todos, setTodos] = useState<Todo[]>([]);
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
 
   useEffect(() => {
     fetch("/api/todo")
@@ -16,6 +39,18 @@ export default function Home() {
       });
   }, []);
 
+  const onSubmit = async (data: z.infer<typeof FormSchema>) => {
+    try {
+      const response = await axios.post("/api/todo", data);
+      setTodos([...todos, response.data]);
+      toast.success("Todo implemented successfully");
+      form.reset();
+    } catch (error) {
+      console.error(error);
+      toast.error("Can't add todo");
+    }
+  };
+
   const onDelete = (id: string) => {
     const newTodos = [...todos].filter((todo) => todo.id !== id);
     setTodos(newTodos);
@@ -23,10 +58,23 @@ export default function Home() {
 
   return (
     <div className="min-h-screen w-full flex justify-center items-center flex-col gap-4">
-      <form className="flex gap-2 w-[400px]">
-        <Input autoFocus className="w-full" />
-        <Button>Save</Button>
-      </form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-[400px] flex gap-4 items-center">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input autoFocus placeholder="Write your todo..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Add</Button>
+        </form>
+      </Form>
       <ul className="pl-4 list-disc w-full max-w-[400px]">
         {todos.map((todo) => (
           <TodoItem onDelete={onDelete} todo={todo} key={todo.id} />
